cb7e225a6ffd9888202bebd09e8cd7480217b2e5
NickBrisebois <nickbrisebois12@gmail.com>
2019-07-11 18:24:50 +0000

    Redid UI, added user list

--- index.js
+++ index.js
@@ -3,25 +3,18 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/views'));
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-io.sockets.on('connection', (socket) => {
-    socket.on('username', (username) => {
-        socket.username = username;
-        io.emit('is_online', '<li>' + socket.username + ' joined the chat</li>');
-    });
-
-    socket.on('disconnect', (username) => {
-        io.emit('is_online', '<i>' + socket.username + ' left the chat</i>');
-    });
-
-    socket.on('chat_message', (message) => {
-        io.emit('chat_message', '<li><strong>' + socket.username + '</strong>: ' + message + '</li>');
-    });
-})
-
-const server = http.listen(8080, () => {
-    console.log('listening on port 8080');
+const server = http.listen(80, () => {
+    console.log('listening on port 80');
 });
