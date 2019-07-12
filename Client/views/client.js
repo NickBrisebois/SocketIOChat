a4d13f99e7ca9a04d9969de0a7636831d2810882
NickBrisebois <nickbrisebois12@gmail.com>
2019-07-12 17:57:17 +0000

    Cleaned up code and did some documentation

--- Client/views/client.js
+++ Client/views/client.js
@@ -3,41 +3,82 @@ let serverUrl = "http://35.183.42.174:8080";
 let socket = io.connect(serverUrl);
 let usernameSet = false;
 
+/*
+ * On new chat message event:
+ * Update the chat box with the new chat message 
+ */
+socket.on('chat_message', function(msgInfo){
+    /* Username must be set before we let users see messages */
+    if(usernameSet) {
+        let msg = craftMsgHTML(msgInfo.user, msgInfo.msg);
+        /* Bottom element is required to always be at the end so that we can scroll to it.
+         * We maintain this by appending html above it and then scrolling it into view */
+        document.getElementById('bottom').insertAdjacentHTML('beforebegin', msg);
+        document.getElementById('bottom').scrollIntoView();
+    }
+});
+
+/*
+ * On new is_online event:
+ * Someone either just went online of offline, print a message with that info
+ */
+socket.on('is_online', function(username) {
+    // Generate HTML for userlist and status message
+    let userStatus = craftStatusMsg(username.user, username.is_online);
+    let userList = craftUserListHTML(username.all_users);
+
+    // Print list of online users
+    document.getElementById('bottom').insertAdjacentHTML('beforebegin', userStatus);
+    document.getElementById('users').innerHTML = userList;
+    document.getElementById('bottom').scrollIntoView();
+});
+
+/**
+ * Returns HTML for a new message that can be used to append to the chat box
+ */
+function craftMsgHTML(username, message) {
+    return "<li class='list-group-item'><strong>" + username + "</strong>: " + message + "</li>";
+}
+
+/*
+ * Returns HTML for the user list
+ */
+function craftUserListHTML(userList) {
+    let listHTML = "";
+    for(let i = 0; i < userList.length; i++) {
+        listHTML += userList[i] + "<hr/>";
+    }
+    return listHTML;
+}
+
+/* 
+ * Craft a status update for when a user joins or leaves
+ */
+function craftStatusMsg(username, isOnline) {
+    let userStatus = (isOnline) ? 'online' : 'offline';
+    return "<li class='list-group-item'>" + username + " is now " + userStatus + ".</li>";
+}
+
+/*
+ * Handler for when a user submits a new message 
+ */
 function submitMessage(e) {
-    e.preventDefault();
+    e.preventDefault(); // Prevent the form from actually being submitted
+    /* If the username hasn't been set then we want to set the username from the message box */
     if(!usernameSet) {
+        /* Set new username */
         usernameSet = true;
         socket.emit('set_username', document.getElementById('txt').value);
         document.getElementById('txt').placeholder="Enter your message";
     }else {
+        /* Send a new message */
         socket.emit('chat_message', document.getElementById('txt').value);
     }
     document.getElementById('txt').value = "";
     return false;
 }
 
+// Add an event handler for when the user hits enter or hits submit
 document.getElementById('submit').addEventListener('click', submitMessage);
 
-// append the chat text message
-socket.on('chat_message', function(msg){
-    if(usernameSet) {
-        msg = "<li class='list-group-item'>" + msg + "</li>";
-        document.getElementById('bottom').insertAdjacentHTML('beforebegin', msg);
-        document.getElementById('bottom').scrollIntoView();
-    }
-});
 
-// append text if someone is online
-socket.on('is_online', function(username) {
-    // Print list of online users
-    let status = (username.is_online) ? 'online' : 'offline';
-    let message = "<li class='list-group-item'>" + username.user + " is now " + status + ".</li>";
-    document.getElementById('bottom').insertAdjacentHTML('beforebegin', message);
-    let userList = "";
-    for(let i = 0; i < username.all_users.length; i++) {
-        userList += username.all_users[i] + "<hr/>";
-    }
-    userList += "";
-    document.getElementById('users').innerHTML = userMotd;
-    document.getElementById('bottom').scrollIntoView();
-});
--- Server/server.js
+++ Server/server.js
@@ -5,10 +5,14 @@ const io = require('socket.io')(http);
 
 let activeUsers = [];
 
+/* On a new connection:
+ * Begin watching for new events
+ */
 io.sockets.on('connection', (socket) => {
-    socket.on('username', (username) => {
-    });
-
+    /*
+     * On user disconnect:
+     * A user has disconnected, remove them from the list of active users
+     */
     socket.on('disconnect', (username) => {
         console.log(socket.username + " disconnected from chat");
         let userIndex = activeUsers.indexOf(socket.username);
@@ -16,13 +20,24 @@ io.sockets.on('connection', (socket) => {
         io.emit('is_online', {'all_users': activeUsers, 'user': socket.username, 'is_online': false});
     });
 
+    /*
+     * On new message:
+     * A user has sent a message to the server, send the message to everyone connected
+     */
     socket.on('chat_message', (message) => {
+        /* Make sure user's message isn't empty (this is also done client side) */
         if(message.length > 0) {
-            io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+            /* Send new 'chat_event' event to all users containing the new message and the sender */
+            io.emit('chat_message', {'user': socket.username, 'msg': message});
         }
     });
     
+    /* 
+     * On username set:
+     * A user has submitted a username, we now add them to the list of active users
+     */
     socket.on('set_username', (username) => {
+        console.log(socket.username + " joined the chat");
         if(username.length > 0) {
             socket.username = username;
             activeUsers.push(username);
